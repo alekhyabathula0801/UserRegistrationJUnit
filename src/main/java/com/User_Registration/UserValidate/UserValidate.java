1f7793c3ae7da2a613ea49fe97faa9437c7f5284
arun kumar <alekhya0801@gmail.com>
2020-04-28 21:45:36 +0530

    Added feature to check mobile number

--- src/main/java/com/User_Registration/UserValidate/UserValidate.java
+++ src/main/java/com/User_Registration/UserValidate/UserValidate.java
@@ -6,15 +6,16 @@ import jdk.nashorn.internal.codegen.Compiler;
 import java.util.regex.Pattern;
 
 public class UserValidate {
-    public static final String NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}";
+    public static final String NAME_PATTERN = "^[A-Z][a-z]{2,}";
     public static final String EMAIL_PATTERN = "^[a-zA-Z0-9]+([._+-][0-9a-zA-Z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})?$";
+    public static final String MOBILE_NUMBER_PATTERN = "^[0-9]{1,3}[ ][0-9]{10}$";
 
-    public boolean checkFirstname(String firstName) {
+    public boolean checkFirstName(String firstName) {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
         return pattern.matcher(firstName).matches();
     }
 
-    public boolean checkLastname(String lastName) {
+    public boolean checkLastName(String lastName) {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
         return pattern.matcher(lastName).matches();
     }
@@ -22,5 +23,10 @@ public class UserValidate {
     public boolean checkEmailId(String emailId) {
         Pattern pattern = Pattern.compile(EMAIL_PATTERN);
         return pattern.matcher(emailId).matches();
+    }
+
+    public boolean checkMobileNumber(String mobileNumber) {
+        Pattern pattern = Pattern.compile(MOBILE_NUMBER_PATTERN);
+        return pattern.matcher(mobileNumber).matches();
     }
 }
--- src/test/java/UserRegistration.java
+++ src/test/java/UserRegistration.java
@@ -6,14 +6,14 @@ public class UserRegistration {
     @Test
     public void givenFirstName_ifProper_thenReturnTrue() {
         UserValidate validator = new UserValidate();
-        boolean result = validator.checkFirstname("Alekhya");
+        boolean result = validator.checkFirstName("Alekhya");
         Assert.assertTrue(result);
     }
 
     @Test
     public void givenLastName_ifProper_thenReturnTrue() {
         UserValidate validator = new UserValidate();
-        boolean result = validator.checkLastname("Bathula");
+        boolean result = validator.checkLastName("Bathula");
         Assert.assertTrue(result);
     }
 
@@ -21,6 +21,13 @@ public class UserRegistration {
     public void givenEmailId_ifProper_thenReturnTrue() {
         UserValidate validator = new UserValidate();
         boolean result = validator.checkEmailId("alekhya0801@gmail.com");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenMobileNumber_ifProper_thenReturnTrue() {
+        UserValidate validator = new UserValidate();
+        boolean result = validator.checkMobileNumber("91 1234567890");
         Assert.assertTrue(result);
     }
 }
